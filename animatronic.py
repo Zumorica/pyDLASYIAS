7ddcdd7a3a1cceeacde469c2f6229d7748febf1d
Zumo <apvictor00@hotmail.com>
2014-09-17 18:31:34 +0200

    Added a few files
    Nothing really :)

---
+++ animatronic.py
@@ -0,0 +1,8 @@
+import sys, time, os, random, time
+
+class animatronic(self, name, slocation, kind, ailvl):
+    def __init__(self):
+        self.name = name
+        self.slocation = slocation #starting location
+        self.kind = kind #For now kinds should be "freddy, "bonnie", "foxy" and "chica"
+        
