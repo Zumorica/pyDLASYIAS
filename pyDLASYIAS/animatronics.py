3b79d8865ba53aff9228ff75f260afb6ac14a513
ZDDM <apvictor00@hotmail.com>
2014-12-20 00:54:49 +0100

    Changed all debugprints to match the new debugprint functionality.

--- pyDLASYIAS/animatronics.py
+++ pyDLASYIAS/animatronics.py
@@ -19,23 +19,21 @@ class animatronic(object): #Animatronics' class.
             self.bseen = False #Being seen. If the camera is active, this variable will be true. Else, it will be false.
 
         if self.ailvl <= 20:
-            debug.debugprint("%s's AI started. -%s BEHAVIOR-" % (self.name.upper(), self.kind.upper()))
+            debug.debugprint("%s's AI started." % (self.name.upper()), self)
             if self.kind == "bear":
                 if self.ailvl > 5:
-                    debug.debugprint("%s's AI IS NOW ACTIVE! -%s BEHAVIOR-" % (self.name.upper(), self.kind.upper()))
+                    debug.debugprint("%s's AI IS NOW ACTIVE!" % (self.name.upper()), self)
                     self.dmove("cam1a")
                 else:
                     self.dmove("off")
 
             thread.start_new_thread(self.think, ()) #Multithreading. This makes the game possible
 
-
-
         else:
-            print "%s's AI LEVEL OVER 20. BE CAREFUL! -%s BEHAVIOR-" % (self.name.upper(), self.kind.upper())
+            print "%s's AI LEVEL IS OVER 20. BE CAREFUL!" % (self.name.upper())
             if self.kind == "bear":
                 if self.ailvl > 5:
-                    debug.debugprint("%s's AI IS NOW ACTIVE! -%s BEHAVIOR-" % (self.name.upper(), self.kind.upper()))
+                    debug.debugprint("%s's AI IS NOW ACTIVE!" % (self.name.upper()), self)
                     self.dmove("cam1a")
                 else:
                     self.dmove("off")
@@ -43,26 +41,29 @@ class animatronic(object): #Animatronics' class.
 
     def dmove(self, room): #Direct move / Debug move. Moves an animatronic to a location.
         self.location = room
-        debug.debugprint("DMoved %s to %s" % (self.name, room))
+        if room == "off":
+            debug.debugprint("%s has been queued for shutdown." % (self.name), self)
+        else:
+            debug.debugprint("DMoved %s to %s" % (self.name, room), self)
         return None
 
     def rmove(self, room): #Random move. Moves an animatronic (or not) to a location.
         self.choice = random.choice(room) #Yup. You can input lists!
-        debug.debugprint("%s's choice was %s -%s BEHAVIOR-" % (self.name, self.choice, self.kind.upper()))
-        if random.randint(0, 20) in range(0, self.ailvl):
+        debug.debugprint("%s's choice was %s" % (self.name, self.choice), self)
+        if random.randint(0, self.ailvl / 2) in range(0, self.ailvl / 2):
             self.location = self.choice
-            debug.debugprint("%s moved to %s -%s BEHAVIOR-" % (self.name, self.choice, self.kind.upper()))
+            debug.debugprint("%s moved to %s" % (self.name, self.choice), self)
             self.think()
             return None
 
         else:
-            debug.debugprint("%s didn't move at all" % (self.name))
+            debug.debugprint("%s didn't move at all" % (self.name), self)
             self.think()
             return None
 
     def think(self):
         if self.location == "off": #Shuts down the AI
-            debug.debugprint("%s's AI is shutting down." % (self.name))
+            debug.debugprint("%s's AI is shutting down." % (self.name), self)
             os.system("exit")
             sys.exit(0)
             os._exit(0)
@@ -70,7 +71,7 @@ class animatronic(object): #Animatronics' class.
         else:
             #Chicken's AI
             if self.kind == "chicken":
-                debug.debugprint("%s is thinking... -CHICKEN BEHAVIOR- -%s-" % (self.name, self.location))
+                debug.debugprint("%s is thinking..." % (self.name), self)
                 time.sleep(random.randint(20, 25) / self.ailvl)
                 if self.location == "cam1a":
                     self.rmove(["cam1b"])
@@ -83,17 +84,18 @@ class animatronic(object): #Animatronics' class.
                 if self.location == "cam4a":
                     self.rmove(["cam1b", "rightdoor", "cam4b", "cam4b"])
                 if self.location == "cam4b":
-                    self.rmove(["cam4a", "rightdoor", "rightdoor", "cam1a"])
+                    self.rmove(["cam4a", "rightdoor", "cam1a"])
                 if self.location == "rightdoor":
-                    debug.debugprint("%s is at rightdoor." % (self.name))
+                    debug.debugprint("%s is at rightdoor." % (self.name), self)
                 if self.location == "inside":
                     pass
                 return None
+                time.sleep(random.randint(20, 25) / self.ailvl)
 
 
             #Rabbit's AI
             if self.kind == "rabbit":
-                debug.debugprint("%s is thinking... -RABBIT BEHAVIOR- -%s-" % (self.name, self.location))
+                debug.debugprint("%s is thinking..." % (self.name), self)
                 time.sleep(random.randint(20, 25) / self.ailvl)
                 if self.location == "cam1a":
                     self.rmove(["cam1b"])
@@ -106,16 +108,17 @@ class animatronic(object): #Animatronics' class.
                 if self.location == "cam3":
                     self.rmove(["cam2a", "leftdoor", "cam2b"])
                 if self.location == "cam2b":
-                    self.rmove(["cam2a", "leftdoor", "leftdoor", "cam1a"])
+                    self.rmove(["cam2a", "leftdoor", "cam1a"])
                 if self.location == "leftdoor":
-                    debug.debugprint("%s is at leftdoor." % (self.name))
+                    debug.debugprint("%s is at leftdoor." % (self.name), self)
                 if self.location == "inside":
                     pass
                 return None
+                time.sleep(random.randint(20, 25) / self.ailvl)
 
             #Fox's AI
             if self.kind == "fox":
-                debug.debugprint("%s is thinking... -FOX BEHAVIOR- -%s-" % (self.name, self.location))
+                debug.debugprint("%s is thinking..." % (self.name), self)
                 time.sleep(random.randint(20, 25) / self.ailvl)
                 if self.foxstatus == 4:
                     time.sleep(100 / self.ailvl)
@@ -123,24 +126,24 @@ class animatronic(object): #Animatronics' class.
 
 
                 if self.foxstatus >= 5:
-                    debug.debugprint("%s is sprinting towards you! -FOX BEHAVIOR- -%s-" % (self.name, self.location))
+                    debug.debugprint("%s is sprinting towards the Security Office." % (self.name), self)
                     time.sleep(20 / self.ailvl)
                     self.location = "cam2a"
                     time.sleep(20 / self.ailvl)
                     self.location = "leftdoor"
 
                 if self.foxtseen >= 1:
-                    debug.debugprint("%s status remains at %s -FOX BEHAVIOR- -%s-" % (self.name, self.foxstatus, self.location))
+                    debug.debugprint("%s status remains at %s." % (self.name, self.foxstatus), self)
                     self.foxsleep += self.foxtseen
                     self.basesleep = random.randint(150, 200) / self.ailvl
                     self.foxtseen = 0
                     time.sleep(self.basesleep + self.foxsleep)
                     if random.randint(0, 1) == 1:
                         self.foxstatus += 1
-                        debug.debugprint("%s status is now %s -FOX BEHAVIOR- -%s-" % (self.name, self.foxstatus, self.location))
+                        debug.debugprint("%s status is now %s." % (self.name, self.foxstatus), self)
                         self.think()
                     else:
-                        debug.debugprint("%s status remains at %s -FOX BEHAVIOR- -%s-" % (self.name, self.foxstatus, self.location))
+                        debug.debugprint("%s status remains at %s." % (self.name, self.foxstatus), self)
                         self.think()
 
 
@@ -148,20 +151,20 @@ class animatronic(object): #Animatronics' class.
                 else:
                     if random.randint(0, 1) == 1:
                         self.foxstatus +=1
-                        debug.debugprint("%s status is now %s" % (self.name, self.foxstatus))
+                        debug.debugprint("%s status is now %s" % (self.name, self.foxstatus), self)
                     else:
-                        debug.debugprint("%s status remains at %s" % (self.name, self.foxstatus))
+                        debug.debugprint("%s status remains at %s" % (self.name, self.foxstatus), self)
                     time.sleep(random.randint(150, 200) / self.ailvl)
                     self.think()
 
             #Bear's AI
             if self.kind == "bear":
                 if self.ailvl > 5:
-                    debug.debugprint("FYI: Bear behavior doesn't *really* work. Be careful around this.")
+                    debug.debugprint("%s is thinking..." % (self.name), self)
                     time.sleep(40 / self.ailvl)
                     if self.location == "cam1a":
                         if self.someoneThere("cam1a") == True:
-                            debug.debugprint("%s.bseen = %s -%s BEHAVIOR-" % (self.name, self.bseen, self.kind.upper()))
+                            debug.debugprint("%s.bseen = %s" % (self.name, self.bseen), self)
                             if self.bseen == False:
                                 self.rmove("cam1b")
                                 print ""
