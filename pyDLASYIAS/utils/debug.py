98a838330448eaef95aa6b9d0e0f305c470cbc55
ZDDM <apvictor00@hotmail.com>
2015-01-04 13:58:14 +0100

    Changed log to Globals.log

--- pyDLASYIAS/utils/debug.py
+++ pyDLASYIAS/utils/debug.py
@@ -1,19 +1,19 @@
 import time
 import pyDLASYIAS.Globals as Globals
 
-log = open("log.txt", "a")
-log.write(time.strftime("\n %d/%m/%Y - %H:%M:%S \n"))
+Globals.log = open("log.txt", "a")
+Globals.log.write(time.strftime("\n %d/%m/%Y - %H:%M:%S \n"))
 
 def debugprint(text, animatronic=None, writetolog=True):
     if writetolog == True:
         if animatronic == None:
-            log.write("%s %s \n" % (text, time.strftime("%H:%M:%S")))
+            Globals.log.write("%s %s \n" % (text, time.strftime("%H:%M:%S")))
         else:
-            log.write("%s -%s BEHAVIOR- -AI LVL: %s- -AGRE.: %s-  -%s- %s \n" % (text, animatronic.kind.upper(), animatronic.ailvl, animatronic.agressiveness, animatronic.location.upper(), time.strftime("%H:%M:%S")))
+            Globals.log.write("%s -%s BEHAVIOR- -AI LVL: %s- -AGRE.: %s-  -%s- %s \n" % (text, animatronic.kind.upper(), animatronic.ailvl, animatronic.agressiveness, animatronic.location.upper(), time.strftime("%H:%M:%S")))
 
     if Globals.debug == True:
         if animatronic == None:
-            print("%s %s \n" % (text, time.strftime("%H:%M:%S")))
+            print("%s -%s USAGE- -%s POWER- -%s TIME IN-GAME- -%s- \n" % (text, time.strftime("%H:%M:%S")))
         else:
             print("%s -%s BEHAVIOR- -AI LVL: %s- -AGRE.: %s- -%s- %s \n" % (text, animatronic.kind.upper(), animatronic.ailvl, animatronic.agressiveness, animatronic.location.upper(), time.strftime("%H:%M:%S")))
 
